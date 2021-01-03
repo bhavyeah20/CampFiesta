6ff26c9422b91d40889f1e88ad6a1c70f8b6a707
Bhavya Chaudhary <bhavyabbps14@gmail.com>
2021-01-04 02:49:13 +0530

    Added Star Ratings

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const { storage } = require('../cloudinary/index');
 const { validateCampground, isLoggedIn, isAuthor } = require('../middleware');
 const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const campgrounds = require('../controllers/campgrounds');
+const upload = multer({ storage })
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files);
+        res.send("It worked");
+    })
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
 router.route('/:id')
