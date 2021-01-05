2378ac37813a01765dd6c2654378c129728780a3
Bhavya Chaudhary <bhavyabbps14@gmail.com>
2021-01-06 00:19:06 +0530

    Added Maps using mapboxgl api

--- models/campground.js
+++ models/campground.js
@@ -5,11 +5,27 @@ const Schema = mongoose.Schema;
 const imageSchema = new Schema({
     url: String,
     filename: String
+});
+
+imageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
 })
+
 
 const CampgroundSchema = new Schema({
     title: String,
     images: [imageSchema],
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
     price: Number,
     description: String,
     location: String,
@@ -25,11 +41,6 @@ const CampgroundSchema = new Schema({
     ]
 });
 
-imageSchema.virtual('thumbnail').get(function () {
-    return this.url.replace('/upload', '/upload/w_200')
-})
-
-//no need to store in db
 
 CampgroundSchema.post('findOneAndDelete', async (doc) => {
     if (doc) {
--- seeds/index.js
+++ seeds/index.js
@@ -24,6 +24,7 @@ const seedDB = async () => {
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            //Your User ID
             author: '5ff1504dff2d69422cc07974',
             images: [
                 {
@@ -35,6 +36,10 @@ const seedDB = async () => {
                     filename: 'YelpCamp/frpg53ygskorijy3ezws'
                 }
             ],
+            geometry: {
+                type: "Point",
+                coordinates: [-113.1331, 47.0202]
+            },
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Veritatis maxime labore eligendi soluta adipisci vel ipsa perspiciatis perferendis iste ullam.Suscipit nobis accusamus tenetur at architecto accusantium facere odit ducimus?',
             price: Math.floor(Math.random() * 20) + 10
         });
